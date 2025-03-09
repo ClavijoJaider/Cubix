c375653c38e2223927887a8e8c901bf4f233aa74
ClavijoJaider <161168718+ClavijoJaider@users.noreply.github.com>
2025-03-09 22:20:31 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Instalar screen y playit
+echo "Instalando screen y playit..."
+sudo apt update
+sudo apt install -y screen curl gpg
+
+# Añadir el repositorio de playit y su clave GPG
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list >/dev/null
+
+# Instalar playit
+sudo apt update
+sudo apt install -y playit
 
 # Limpiar sesiones de screen obsoletas
 screen -wipe
 
+# Reiniciar sesión 'mods'
 echo "Reiniciando sesión 'mods'..."
 screen -dmS mods /bin/bash -c 'cd Mods && chmod +x run.sh && ./run.sh'
 
+# Reiniciar sesión 'playit'
 echo "Reiniciando sesión 'playit'..."
 screen -dmS playit /bin/bash -c 'playit'
-
